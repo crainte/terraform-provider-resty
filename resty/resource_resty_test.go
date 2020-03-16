adbecdbac61666d154f7182ebab17ae15359bda5
Chris Alexander <crainte@hybridoptix.net>
2020-03-16 14:26:53 -0500

    Add tests to filter

--- resty/resource_resty_test.go
+++ resty/resource_resty_test.go
@@ -1,32 +1,33 @@
 package resty
 
 import (
-    "fmt"
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-    "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 type testHttpMock struct {
-    server *httptest.Server
+	server *httptest.Server
 }
 
 const testResourceConfig = `
 resource "resty" "test" {
-  url = "%s/test"
+  url    = "%s/test"
   method = "GET"
 }
 `
 
 func TestResourceGet(t *testing.T) {
-    mock := initMockHttpServer()
+	mock := initMockHttpServer()
 
-    defer mock.server.Close()
+	defer mock.server.Close()
 
-    resource.UnitTest(t, resource.TestCase{
+	resource.UnitTest(t, resource.TestCase{
 		Providers: testProviders,
 		Steps: []resource.TestStep{
 			{
@@ -37,7 +38,7 @@ func TestResourceGet(t *testing.T) {
 						return fmt.Errorf("missing resty resource 'test'")
 					}
 
-                    r := s.RootModule().Resources["resty.test"].Primary.Attributes
+					r := s.RootModule().Resources["resty.test"].Primary.Attributes
 
 					if r["response"] != "{}" {
 						return fmt.Errorf(
@@ -62,8 +63,8 @@ func TestResourceGet(t *testing.T) {
 
 const testResourceConfigHeaders = `
 resource "resty" "test" {
-  url = "%s/headers"
-  method = "GET"
+  url     = "%s/headers"
+  method  = "GET"
   headers = {
     "Authorization" = "ZGVhZDpiZWVmCg=="
   }
@@ -71,11 +72,11 @@ resource "resty" "test" {
 `
 
 func TestResourceGet_withHeaders(t *testing.T) {
-    mock := initMockHttpServer()
+	mock := initMockHttpServer()
 
-    defer mock.server.Close()
+	defer mock.server.Close()
 
-    resource.UnitTest(t, resource.TestCase{
+	resource.UnitTest(t, resource.TestCase{
 		Providers: testProviders,
 		Steps: []resource.TestStep{
 			{
@@ -86,7 +87,7 @@ func TestResourceGet_withHeaders(t *testing.T) {
 						return fmt.Errorf("missing resty resource 'test'")
 					}
 
-                    r := s.RootModule().Resources["resty.test"].Primary.Attributes
+					r := s.RootModule().Resources["resty.test"].Primary.Attributes
 
 					if r["response"] != "{}" {
 						return fmt.Errorf(
@@ -95,11 +96,63 @@ func TestResourceGet_withHeaders(t *testing.T) {
 						)
 					}
 
-                    if ok := r["headers.Authorization"] == "ZGVhZDpiZWVmCg==" ; ! ok {
-                        return fmt.Errorf(
-                            "'headers.Authorization' not stored correctly in state",
-                        )
-                    }
+					if ok := r["headers.Authorization"] == "ZGVhZDpiZWVmCg=="; !ok {
+						return fmt.Errorf(
+							"'headers.Authorization' not stored correctly in state",
+						)
+					}
+
+					return nil
+				},
+			},
+		},
+	})
+}
+
+const testResourceConfigFilter = `
+resource "resty" "test" {
+  url     = "%s/filter"
+  method  = "GET"
+  headers = {
+    "Authorization" = "ZGVhZDpiZWVmCg=="
+  }
+  key = "content"
+  filter {
+    name  = "interesting"
+    value = "value"
+  }
+}
+`
+
+func TestResourceGet_withFilter(t *testing.T) {
+	mock := initMockHttpServer()
+
+	defer mock.server.Close()
+
+	resource.UnitTest(t, resource.TestCase{
+		Providers: testProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testResourceConfigFilter, mock.server.URL),
+				Check: func(s *terraform.State) error {
+					_, ok := s.RootModule().Resources["resty.test"]
+					if !ok {
+						return fmt.Errorf("missing resty resource 'test'")
+					}
+
+					r := s.RootModule().Resources["resty.test"].Primary.Attributes
+
+					m := make(map[string]interface{})
+					if err := json.Unmarshal([]byte(r["response"]), &m); err != nil {
+						return fmt.Errorf("Unable to parse JSON")
+					}
+
+					if m["working"].(string) != "yes" {
+						return fmt.Errorf(
+							`'response' output is %s; want '{"working": "yes"}'`,
+							r["response"],
+						)
+					}
 
 					return nil
 				},
@@ -116,7 +169,7 @@ func initMockHttpServer() *testHttpMock {
 			w.Header().Add("X-Is-Teapot", "Yes")
 			if r.URL.Path == "/test" {
 				w.WriteHeader(http.StatusOK)
-                w.Write([]byte("{}"))
+				w.Write([]byte("{}"))
 			} else if r.URL.Path == "/headers" {
 				if r.Header.Get("Authorization") == "ZGVhZDpiZWVmCg==" {
 					w.WriteHeader(http.StatusOK)
@@ -124,16 +177,13 @@ func initMockHttpServer() *testHttpMock {
 				} else {
 					w.WriteHeader(http.StatusForbidden)
 				}
-			} else if r.URL.Path == "/utf-8/meta_200.txt" {
-				w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("1.0.0"))
-			} else if r.URL.Path == "/utf-16/meta_200.txt" {
-				w.Header().Set("Content-Type", "application/json; charset=UTF-16")
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("\"1.0.0\""))
-			} else if r.URL.Path == "/meta_404.txt" {
-				w.WriteHeader(http.StatusNotFound)
+			} else if r.URL.Path == "/filter" {
+				if r.Header.Get("Authorization") == "ZGVhZDpiZWVmCg==" {
+					w.WriteHeader(http.StatusOK)
+					w.Write([]byte("{\"content\": [{\"interesting\": \"no\", \"you\": \"failed\"},{\"interesting\": \"value\", \"working\": \"yes\"}]}"))
+				} else {
+					w.WriteHeader(http.StatusForbidden)
+				}
 			} else {
 				w.WriteHeader(http.StatusNotFound)
 			}
